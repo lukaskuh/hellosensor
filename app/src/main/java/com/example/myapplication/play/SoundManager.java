7c10fbc7f6f1c31a285b84ab4d1cebca813b0822
Lukas K <lukaskuhlin@gmail.com>
2025-02-06 17:09:08 +0100

    More sounds

--- app/src/main/java/com/example/myapplication/play/SoundManager.java
+++ app/src/main/java/com/example/myapplication/play/SoundManager.java
@@ -7,8 +7,20 @@ import android.media.ToneGenerator;
 public class SoundManager {
     ToneGenerator toneGenerator = new ToneGenerator(STREAM_MUSIC, ToneGenerator.MAX_VOLUME);
 
-    public void playBeep() {
+    public void playBeepLow() {
         toneGenerator.startTone(ToneGenerator.TONE_DTMF_A, 200);
+    }
+
+    public void playBeepHigh() {
+        toneGenerator.startTone(ToneGenerator.TONE_DTMF_C, 200);
+    }
+
+    public void playBlastOff() {
+        toneGenerator.startTone(ToneGenerator.TONE_DTMF_D, 600);
+    }
+
+    public void playFinish() {
+        toneGenerator.startTone(ToneGenerator.TONE_DTMF_A, 600);
     }
 
 }
